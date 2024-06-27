334ab6f2901697e26fc8cec1b7f9ad2a13283897
dan <dnbsd@posteo.net>
2024-06-27 21:36:51 +0200

    Add urfave/cli/v2 to handle command-line arguments and subcommands

--- cmd/fzz/main.go
+++ cmd/fzz/main.go
@@ -1,37 +1,119 @@
 package main
 
 import (
-	"context"
+	"errors"
+	"fmt"
 	"github.com/foojank/fzz"
 	"github.com/foojank/fzz/services/builder"
 	"github.com/foojank/fzz/services/executor"
+	"github.com/urfave/cli/v2"
 	"os"
+	"strings"
 )
 
-func main() {
-	switch os.Args[1] {
-	case "build":
-		err := fzz.Build(context.TODO(), os.Args[2], builder.Arguments{
-			Output: "fizzpit.fzz",
-		})
-		if err != nil {
-			panic(err)
-		}
-	case "exec":
-		b, err := os.ReadFile(os.Args[2])
-		if err != nil {
-			panic(err)
-		}
+var version string
 
-		err = fzz.Exec(context.TODO(), b, executor.Arguments{
-			Command:      os.Args[3],
-			Unrestricted: false,
-			Imports: []fzz.Imports{
-				fzz.Stdlib,
+func Version() string {
+	return strings.TrimSpace(version)
+}
+
+var app = cli.App{
+	Name:           "fzz",
+	Usage:          "Build or run fizzpit files",
+	Args:           true,
+	Version:        Version(),
+	DefaultCommand: "",
+	Commands: cli.Commands{
+		{
+			Name:   "build",
+			Usage:  "Build fizzpit from a source directory",
+			Args:   true,
+			Action: build,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "o",
+					Value: "o.fzz",
+					Usage: "output file",
+				},
 			},
-		})
-		if err != nil {
-			panic(err)
-		}
+			HideHelpCommand: true,
+		},
+		{
+			Name:   "run",
+			Usage:  "Run fizzpit file",
+			Args:   true,
+			Action: run,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "f",
+					Required: true,
+					Usage:    "input file",
+				},
+			},
+			HideHelpCommand: true,
+		},
+	},
+}
+
+func build(c *cli.Context) error {
+	args := c.Args()
+	if !args.Present() {
+		return errors.New("no argument specified")
+	}
+
+	src := args.First()
+	out := c.String("o")
+	err := fzz.Build(c.Context, src, builder.Arguments{
+		Output: out,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func run(c *cli.Context) error {
+	args := c.Args()
+	if !args.Present() {
+		return errors.New("no argument specified")
+	}
+
+	file := c.String("f")
+	b, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	cmd := args.Get(0)
+	cmdArgs := make([]string, 0, args.Len()-1)
+	for i := 1; i < args.Len(); i++ {
+		cmdArgs = append(cmdArgs, args.Get(i))
+	}
+
+	err = fzz.Exec(c.Context, b, executor.Arguments{
+		Command:      cmd,
+		Args:         cmdArgs,
+		Env:          nil,
+		Unrestricted: true, // TODO: disable by default, configurable!
+		Imports: []fzz.Imports{
+			fzz.Stdlib,
+			fzz.Unrestricted, // TODO: disable by default, configurable!
+			fzz.Syscall,      // TODO: disable by default, configurable!
+			fzz.Unsafe,       // TODO: disable by default, configurable!
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,14 @@ go 1.22.2
 require (
 	github.com/otiai10/copy v1.14.0
 	github.com/traefik/yaegi v0.16.1
+	github.com/urfave/cli/v2 v2.27.2
 	golang.org/x/mod v0.18.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 )
